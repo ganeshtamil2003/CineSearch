a2786a818b7450fee184a269695b3ffcc7b91985
ganeshtamil2003 <ganesh28062003@gmail.com>
2024-02-28 23:21:28 +0530

    cineSearch project created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./components/MovieCard";
 
-function App() {
+// import { useState, useEffect } from 'react';
+
+//-> PROPS example
+// const Person = (props) => {
+//   return (
+//     <>
+//       <h1>Name : {props.name} </h1>
+//       <h2>Last Name :{props.lastname} </h2>
+//       <h3>Age : {props.age} </h3>
+//     </>
+
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div className="App">
+
+//       {/* <Person
+//         name='Rajesh'
+//         lastname='tamil'
+//         age={26}
+//       />
+//       <Person
+//         name='Ganesh'
+//         lastname='Ram'
+//         age={20} 
+//       /> */}
+//     </div>
+
+//   );
+// }
+
+
+// States example
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+
+// useEffect(()=>{
+//  alert("You've Changed the Counter to "+ counter);
+// },[counter])
+
+// return (
+//     <div className="App">
+//       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+//       <h1>{counter} </h1>
+//       <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+//     </div>
+//   )
+// }
+
+
+// 71c002bd --> api key
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=71c002bd';
+
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+
+  }
+
+  useEffect(() => {
+    searchMovies('spiderman')
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>CineSearch</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) :
+          (
+            <div className="empty">
+              <h2>No movies Found</h2>
+            </div>
+          )
+      }
+
+
+
     </div>
   );
 }
+
 
 export default App;
